dd1805330f300e993a93fdfea56bf4b813ea5de5
MelvinRitfeld <melvinterryxfire@gmail.com>
2026-03-12 17:42:14 -0300

    fix: correct frontend URL in CORS

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:3000",
-    "https://renewed-recreation-production-2702.up.railway.app/",],
+    "https://renewed-recreation-production-a5fa.up.railway.app/",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
